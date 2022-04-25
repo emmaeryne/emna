474a041f48ee012b9007c617814a3978b97b6537
emmaeryne <94049201+emmaeryne@users.noreply.github.com>
2022-04-25 20:16:58 +0100

    Update ThemeParkRideController.java

--- ThemeParkRideController.java
+++ ThemeParkRideController.java
@@ -1,15 +1,18 @@
-package project.architecture.ThemeParkApplication.controller;
+package project.architecture.entity;
 
-import com.tomgregory.entity.ThemeParkRide;
-import com.tomgregory.repository.ThemeParkRideRepository;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.server.ResponseStatusException;
+import project.architecture.repository.ThemeParkRideRepository;
 
 import javax.validation.Valid;
+import java.awt.*;
 
-@RestController
 public class ThemeParkRideController {
     private final ThemeParkRideRepository themeParkRideRepository;
 
@@ -17,18 +20,27 @@ public class ThemeParkRideController {
         this.themeParkRideRepository = themeParkRideRepository;
     }
 
-    @GetMapping(value = "/ride", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Iterable<ThemeParkRide> getRides() {
+    @GetMapping(value="/ride", produces=
+
+            PageAttributes.MediaType.APPLICATION_JSON_VALUE)
+    public Iterable<ThemeParkRide> getRide(){
         return themeParkRideRepository.findAll();
-    }
 
-    @GetMapping(value = "/ride/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ThemeParkRide getRide(@PathVariable long id){
-        return themeParkRideRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Invalid ride id %s", id)));
     }
+    @GetMapping (value= "/ride/{id}",produces=
+            PageAttributes.MediaType.APPLICATION_JSON_VALUE)
 
-    @PostMapping(value = "/ride", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ThemeParkRide createRide(@Valid @RequestBody ThemeParkRide themeParkRide) {
-        return themeParkRideRepository.save(themeParkRide);
+    public ThemeParkRide getRide (@PathVariable long id){
+        return  themeParkRideRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("invalide ride id%",id)));
+
+
     }
+@PostMapping(value = "/ride", consumes =
+
+        MediaType.APPLICATION_JSON_VALUE,produces =
+MediaType.APPLICATION_JSON_VALUE)
+    public ThemeParkRide createRide(@Valid @RequestBody ThemeParkRide){
+return themeParkRideRepository.save(new ThemeParkRide());
+}
+
 }
