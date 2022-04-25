7330e53c39165fcab186b220ca5bb1fee711e894
emmaeryne <94049201+emmaeryne@users.noreply.github.com>
2022-04-25 20:17:34 +0100

    Update ThemeParkRideRepository.java

--- ThemeParkRideRepository.java
+++ ThemeParkRideRepository.java
@@ -1,11 +1,8 @@
-package project.architecture.ThemeParkApplication.repository;
-
-import project.architecture.ThemeParkApplication.entity.ThemeParkRide;
+package project.architecture.repository;
+import project.architecture.entity.ThemeParkRide;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
-
 @Repository
 public interface ThemeParkRideRepository extends CrudRepository<ThemeParkRide, Long> {
     List<ThemeParkRide> findByName(String name);
