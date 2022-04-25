c8952ab51c6e209aa627ad58f0cfc7823179a413
emmaeryne <94049201+emmaeryne@users.noreply.github.com>
2022-04-25 20:15:42 +0100

    Update ThemeParkApplicationIT.java

--- ThemeParkApplicationIT.java
+++ ThemeParkApplicationIT.java
@@ -1,5 +1,4 @@
 package project.architecture.ThemeParkApplication;
-
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
@@ -16,22 +15,22 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 @SpringBootTest
 @AutoConfigureMockMvc
 public class ThemeParkApplicationIT {
-
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     public void getsAllRides() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/ride")
-                .accept(MediaType.APPLICATION_JSON))
+                        .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andReturn();
-    }
 
+
+}
     @Test
     public void getsSingleRide() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/ride/1")
-                .accept(MediaType.APPLICATION_JSON))
+                        .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andReturn();
     }
@@ -39,7 +38,7 @@ public class ThemeParkApplicationIT {
     @Test
     public void returnsNotFoundForInvalidSingleRide() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/ride/4")
-                .accept(MediaType.APPLICATION_JSON))
+                        .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotFound())
                 .andReturn();
     }
@@ -48,9 +47,9 @@ public class ThemeParkApplicationIT {
     public void addsNewRide() throws Exception {
         String newRide = "{\"name\":\"Monorail\",\"description\":\"Sedate travelling ride.\",\"thrillFactor\":2,\"vomitFactor\":1}";
         mockMvc.perform(MockMvcRequestBuilders.post("/ride")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(newRide)
-                .accept(MediaType.APPLICATION_JSON))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(newRide)
+                        .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andReturn();
     }
